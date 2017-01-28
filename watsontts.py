d48429164550cbae1dc18dcaff90cd17e976a3a8
Barend <bvanrijn@users.noreply.github.com>
2017-01-28 12:35:07 +0100

    Explain what I'm doing

--- watsontts.py
+++ watsontts.py
@@ -13,23 +13,29 @@ syn_parameters = {
 }
 
 def main():
+    # Get the text
     text = input('Text: ')
 
+    # Synthesize it
     s = synthesize(text)
 
+    # Write it to a `f.ogg` file, overwriting it if it exists
     with open('f.ogg', 'wb+') as f:
         f.write(s.content)
 
 
 def synthesize(text):
+    # Don't modify the original object
     parameters = copy.copy(syn_parameters)
     
+    # Set the text
     parameters['text'] = text
 
+    # Request foo!
     r = requests.get('https://text-to-speech-demo.mybluemix.net/api/synthesize?' + urlencode(parameters))
 
     return r
 
-
 if __name__ == '__main__':
+    # watsontts was called directly
     main()
